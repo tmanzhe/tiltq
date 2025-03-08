a46a19543e60a07e7ec304140c81fc1b213c89d2
tmanzhe <trumanzhe@gmail.com>
2025-03-08 14:14:09 -0500

    updated analytics tracking again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from 'next/script';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -59,19 +60,6 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-NBFQPSRQ');`}
         </Script>
         {/* End Google Tag Manager */}
-        {/* Google tag (gtag.js) */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=G-G2VV5BNMN2`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-G2VV5BNMN2');
-          `}
-        </Script>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/tiltqimage.png" type="image/png" sizes="32x32" />
         <link rel="icon" href="/tiltqimage.svg" type="image/svg+xml" />
@@ -92,6 +80,8 @@ export default function RootLayout({
         {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
+        <GoogleAnalytics gaId="G-G2VV5BNMN2" />
+        <GoogleAnalytics gaId="G-2EHCP8TKD3" />
       </body>
     </html>
   );
